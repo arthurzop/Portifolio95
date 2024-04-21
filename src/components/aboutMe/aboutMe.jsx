8e3b869ce0a2b380c3969b053b45e8af3943cd21
arthurzop <medeirosartur48@gmail.com>
2024-04-21 16:22:04 -0300

    finished about me window

--- src/components/aboutMe/aboutMe.jsx
+++ src/components/aboutMe/aboutMe.jsx
@@ -25,9 +25,9 @@ export default function AboutMe({ logic }) {
           position: "absolute",
           top: "10%",
           left: "30%",
-          width: 'fit-content',
-          height: 'fit-content',
-          userSelect: 'text'
+          width: 650,
+          height: "fit-content",
+          userSelect: "text",
         }}
       >
         <R.WindowHeader
@@ -48,34 +48,38 @@ export default function AboutMe({ logic }) {
                 cursor: "pointer",
                 paddingInline: 15,
                 marginInline: 2,
-                textWrap: 'nowrap' 
+                textWrap: "nowrap",
               }}
             >
-              Know me
+              Overview
             </R.Tab>
             <R.Tab
               value={1}
-              style={{ cursor: "pointer", paddingInline: 15, marginInline: 2, textWrap: 'nowrap' }}
+              style={{
+                cursor: "pointer",
+                paddingInline: 15,
+                marginInline: 2,
+                textWrap: "nowrap",
+              }}
             >
-              Currently Doing
+              Working On
             </R.Tab>
             <R.Tab
               value={2}
-              style={{ cursor: "pointer", paddingInline: 15, marginInline: 2, textWrap: 'nowrap' }}
+              style={{
+                cursor: "pointer",
+                paddingInline: 15,
+                marginInline: 2,
+                textWrap: "nowrap",
+              }}
             >
-              My Focus
-            </R.Tab>
-            <R.Tab
-              value={3}
-              style={{ cursor: "pointer", paddingInline: 15, marginInline: 2, textWrap: 'nowrap' }}
-            >
-              Socials
+              Skills
             </R.Tab>
           </R.Tabs>
           <R.TabBody>
             {activeTab === 0 && (
               <R.GroupBox
-                label='About Me'
+                label="About Me"
                 style={{
                   display: "flex",
                   gap: 20,
@@ -86,16 +90,83 @@ export default function AboutMe({ logic }) {
                 <div className="about-text">
                   <h1>Artur Medeiros</h1>
                   <p>São Paulo, Brasil</p>
-                  <R.Divider/>
-                  <h2>I'm 20 years old, full time student at Senai.I love to design & code in my free time.</h2>
+                  <R.Divider />
+                  <h2>
+                    I'm 20 years old, full time student at Senai-SP. Front-End &
+                    Design focused.
+                  </h2>
                   <h3>Welcome to my Portfolio!</h3>
-                  
                 </div>
               </R.GroupBox>
             )}
-            {activeTab === 1 && <R.GroupBox>1</R.GroupBox>}
-            {activeTab === 2 && <R.GroupBox>2</R.GroupBox>}
-            {activeTab === 3 && <R.GroupBox>3</R.GroupBox>}
+            {activeTab === 1 && (
+              <R.GroupBox label="Working On">
+                <div className="currently-text">
+                  <h2>Studying:</h2>
+                  <ul>
+                    <li>React</li>
+                    <li>PHP</li>
+                    <li>Web Design</li>
+                    <li>UI/UX Interface</li>
+                  </ul>
+                  <R.Divider />
+                  <h2>Projects:</h2>
+                  <ul>
+                    <li>Personal Portfolio</li>
+                    <li>
+                      Final Project (React)
+                      <li>
+                        <R.Anchor
+                          href="https://github.com/arthurzop/tcc-front.git"
+                          target="_blank"
+                        >
+                          Github Repo
+                        </R.Anchor>
+                      </li>
+                    </li>
+                    <li>
+                      Mobile Applications (React Native)
+                      <li>
+                        <R.Anchor
+                          href="https://github.com/arthurzop/calcChurras-native.git"
+                          target="_blank"
+                        >
+                          Github Repo
+                        </R.Anchor>
+                      </li>
+                    </li>
+                  </ul>
+                </div>
+              </R.GroupBox>
+            )}
+            {activeTab === 2 && (
+              <R.GroupBox label="Skills">
+                <div className="currently-text">
+                  <h2>Languages & Frameworks:</h2>
+                  <ul>
+                    <li>JavaScript</li>
+                    <li>ReactJS</li>
+                    <li>React Native</li>
+                    <li>Styled Components</li>
+                    <li>HTML / CSS</li>
+                    <li>BootStrap</li>
+                  </ul>
+                  <R.Divider />
+                  <h2>Tech:</h2>
+                  <ul>
+                    <li>Git</li>
+                    <li>Figma</li>
+                    <li>Notion</li>
+                  </ul>
+                  <R.Divider />
+                  <h2>Agile Methodology:</h2>
+                  <ul>
+                    <li>Scrum</li>
+                    <li>Kanbam</li>
+                  </ul>
+                </div>
+              </R.GroupBox>
+            )}
           </R.TabBody>
         </R.WindowContent>
       </R.Window>
