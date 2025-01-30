37246eb7372bf3cb717d995215d21806186e1e70
arthurzop <medeirosartur48@gmail.com>
2025-01-30 16:19:42 -0300

    update on newer and bigger projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -6,10 +6,9 @@ import * as R from "react95";
 import project1 from "../../assets/project1.png";
 import project2 from "../../assets/project2.png";
 import project3 from "../../assets/project3.png";
-import project4 from "../../assets/project4.png";
 import project5 from "../../assets/project5.png";
-import project6 from "../../assets/project6.png";
 import comic from "../../assets/comic.gif";
+import link from "../../assets/link.svg";
 
 export default function Projects({ logic, handle }) {
   const [state, setState] = useState({
@@ -46,7 +45,7 @@ export default function Projects({ logic, handle }) {
           </R.WindowHeader>
         </div>
         <R.WindowContent>
-          <R.Tabs value={activeTab} onChange={handleChange}>
+          <R.Tabs rows={2} value={activeTab} onChange={handleChange}>
             <R.Tab
               value={0}
               style={{
@@ -67,7 +66,7 @@ export default function Projects({ logic, handle }) {
                 textWrap: "nowrap",
               }}
             >
-              TCC SENAI
+              Prof Correa (em progresso)
             </R.Tab>
             <R.Tab
               value={2}
@@ -78,7 +77,7 @@ export default function Projects({ logic, handle }) {
                 textWrap: "nowrap",
               }}
             >
-              Landing page SOS
+              Luiz Brito
             </R.Tab>
             <R.Tab
               value={3}
@@ -111,7 +110,7 @@ export default function Projects({ logic, handle }) {
                 textWrap: "nowrap",
               }}
             >
-              Esse portfolio!
+              Code Trouble
             </R.Tab>
           </R.Tabs>
           <R.TabBody>
@@ -129,14 +128,17 @@ export default function Projects({ logic, handle }) {
                   <h1>Bem vindo aos meus projetos!</h1>
                   <R.Divider />
                   <h2>
-                    Nessa sessao vc vai poder ver algum dos meus projetos, mas
-                    apenas os destaques. :v
+                    Nesta janela voce vai poder ver alguns dos meus projetos,
+                    nas areas de Web Design e Brand Design.
                     <br />
                     Voce pode ver mais no meu {""}
                     <R.Anchor href="https://github.com/arthurzop">
-                      github
+                      github <img src={link} alt="" className="link" />
                     </R.Anchor>{" "}
-                    ,ou entrar em contato, se quiser!
+                    ou no meu {""}
+                    <R.Anchor href="https://behance.net/arthurzop">
+                      behance <img src={link} alt="" className="link" />
+                    </R.Anchor>{" "}
                   </h2>
                   <img src={comic} alt="" className="comic" />
                 </div>
@@ -144,7 +146,7 @@ export default function Projects({ logic, handle }) {
             )}
             {activeTab === 1 && (
               <R.GroupBox
-                label=""
+                label="work in progress..."
                 style={{
                   display: "flex",
                   gap: 20,
@@ -153,29 +155,27 @@ export default function Projects({ logic, handle }) {
                 <div className="project-container">
                   <div className="image-container">
                     <img src={project1} alt="" className="project-image" />
-                    <img src={project2} alt="" className="project-image" />
+                    {/* <img src={project2} alt="" className="project-image" /> */}
                   </div>
                   <div className="text-container">
-                    <h1>Espaco Maker 3D</h1>
+                    <h1>Identidade Visual: Prof Correa</h1>
                     <R.Divider />
                     <h2>
-                      Esse e o projeto final do senai, para conclusao do curso.
-                      Ele e um sistema que permite os estudantes agendem e
-                      imprimam usando as impressoras 3d da escola.
+                      Esse e o projeto no qual estou trabalhando atualmente,
+                      finalizando a identidade visual e partindo para o web
+                      design do portifolio do meu chefinho.
+                      <br />
+                      <br />
+                      Tentamos implementar muito da heranca africana dele nessa
+                      identidade, aplicando tanto na logo quanto na paleta de
+                      cores.
                     </h2>
                     <h2>
                       <R.Anchor
-                        href="https://github.com/arthurzop/tcc-front.git"
+                        href="https://www.behance.net/gallery/218017203/Prof-Correa-Brand-Design"
                         target="_blank"
                       >
-                        repositorio no github
-                      </R.Anchor>
-                      <br />
-                      <R.Anchor
-                        href="https://www.figma.com/file/qDJCO4VNlhh9HOTGCemF69/TCC?type=design&node-id=0%3A1&mode=design&t=BPq1wcYIyGpSIVHH-1"
-                        target="_blank"
-                      >
-                        projeto no figma
+                        projeto completo no behance
                       </R.Anchor>
                     </h2>
                   </div>
@@ -193,30 +193,28 @@ export default function Projects({ logic, handle }) {
                 <div className="project-container">
                   <div className="image-container">
                     <img src={project3} alt="" className="project-image" />
-                    <img src={project4} alt="" className="project-image" />
                   </div>
                   <div className="text-container">
-                    <h1>SOS - Landing page</h1>
+                    <h1>
+                      Identidade visual & Web design: <br /> Luiz Brito
+                    </h1>
                     <R.Divider />
                     <h2>
-                      Ja esse projeto e focado 100% no design, fiz para minhas
-                      aulas de web design. é uma landing page baseada no album
-                      sos, da sza. Nesse daqui deixei minha imaginacao correr
-                      livre para criar mais uma 'arte' do que um site.
+                      Nesse projeto fui contratado por outro ex-aluno do SENAI
+                      para desenvolver uma identidade visual e prototipar o
+                      portfólio dele.
+                      <br />
+                      Para a identidade visual criei a logo e uma paleta
+                      condizente com a area dele (DevOps e Cloud Architect), ja
+                      para o site em sí projetei o light/dark mode com uma
+                      estetica moderna.
                     </h2>
                     <h2>
                       <R.Anchor
-                        href="https://github.com/arthurzop/sos.git"
+                        href="https://www.behance.net/gallery/218014731/Luiz-Brito-Branding-Web-Design"
                         target="_blank"
                       >
-                        repositorio no github
-                      </R.Anchor>
-                      <br />
-                      <R.Anchor
-                        href="https://www.figma.com/file/IYA9A4wRi4m05ywpyVd9O5/SOS-landing?type=design&mode=design&t=BPq1wcYIyGpSIVHH-1"
-                        target="_blank"
-                      >
-                        projeto no figma
+                        projeto completo no behance
                       </R.Anchor>
                     </h2>
                   </div>
@@ -225,7 +223,6 @@ export default function Projects({ logic, handle }) {
             )}
             {activeTab === 3 && (
               <R.GroupBox
-                label="work in progress..."
                 style={{
                   display: "flex",
                   gap: 20,
@@ -234,16 +231,22 @@ export default function Projects({ logic, handle }) {
                 <div className="project-container">
                   <div className="image-container">
                     <img src={project5} alt="" className="project-image" />
-                    <img src={project6} alt="" className="project-image" />
                   </div>
                   <div className="text-container">
-                    <h1>Suporte de Domingo</h1>
+                    <h1>
+                      Identidade visual e Web Design: <br />
+                      Suporte de Domingo
+                    </h1>
                     <R.Divider />
                     <h2>
                       A suporte foi criada pelo meu ex-professor (e hoje chefe)
                       com o intuito de criar uma forte comunidade de devs e
-                      designers. Eu criei o desing da marca suporte e todo o
-                      web-design das páginas.
+                      designers.
+                      <br />
+                      <br />
+                      Eu tive a oportunidade de desenvolver toda a identidade
+                      visual (alem do símbolo) e prototipar o website responsivo
+                      da Suporte.
                     </h2>
                     <h2>
                       <R.Anchor
@@ -266,7 +269,6 @@ export default function Projects({ logic, handle }) {
             )}
             {activeTab === 4 && (
               <R.GroupBox
-                label="work in progress..."
                 style={{
                   display: "flex",
                   gap: 20,
@@ -276,13 +278,13 @@ export default function Projects({ logic, handle }) {
                   <h1>Data Mastery</h1>
                   <R.Divider />
                   <h2>
-                    Também para um ex-professor do SENAI, estou tendo a
-                    oportunidade de participar como web designer na craição da
-                    landing page e outras páginas em conjunto para sua empresa
+                    Tambem para um ex-professor do SENAI, estou tendo a
+                    oportunidade de participar como web designer na craicao da
+                    landing page e outras paginas em conjunto para sua empresa
                     de ciencia de dados e power BI.
                     <br />
                     Nesse projeto e na Suporte de Domingo estou exclusivamente
-                    na área do web design, sendo assim nao estou codando o
+                    na area do web design, sendo assim nao estou codando o
                     front-end.
                   </h2>
                 </div>
@@ -290,21 +292,33 @@ export default function Projects({ logic, handle }) {
             )}
             {activeTab === 5 && (
               <R.GroupBox>
-                <div className="text-container">
-                  <h1>Esse portfolio por si so!!</h1>
-                  <R.Divider />
-                  <h2>
-                    Eu to muito muito orgulhoso desse aqui. Eu ja conhecia a lib
-                    react95 fazia alguns meses mas nunca tive a oportunidade de
-                    usar ela, ate chegar na ideia de usar ela para criar meu
-                    proprio portfolio pessoal :) e eu, sendo eu, coloquei meu
-                    maximo nesse projeto aqui.
-                    <br />
-                    <br />
-                    Eu amo essa estetica de internet antiga e computadores
-                    antigos! Entao poder usar para expressar meu trabalho, meus
-                    gostos e minha estetica é bem recompensador!
-                  </h2>
+                <div className="project-container">
+                  <div className="image-container">
+                    <img src={project2} alt="" className="project-image" />
+                  </div>
+                  <div className="text-container">
+                    <h1>
+                      Identidade visual, prototipacao web e mobile:{" "}
+                      <br />
+                      Code Trouble
+                    </h1>
+                    <R.Divider />
+                    <h2>
+                      Acho que o maior projeto que ja desenvolvi enquanto Web
+                      Designer. A code Trouble foi pensada como uma plataforma
+                      brasileira de interacao entre desenvolvedores.
+                      <br />
+                      <br />
+                      Nesse projeto eu fiquei responsavel por desenvolver uma
+                      identidade visual concisa, que aguentasse o enorme tamanho
+                      do website E do aplicativo que seriam criados a partir
+                      dela.
+                      <br />
+                      Inclsuive, tambem prototipipamos todas as telas do website
+                      e do aplicativo mobile, levando como base do projeto o
+                      conceito de Atomic Design.
+                    </h2>
+                  </div>
                 </div>
               </R.GroupBox>
             )}
